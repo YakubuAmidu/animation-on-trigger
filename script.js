6013134118fb242591d253955b1605384c69dacd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2023-06-19 03:21:14 -0700

    Initial commit

--- script.js
+++ script.js
@@ -8,38 +8,59 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y){
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+         this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'img/boom1.png';
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;
     };
 
     update(){
-        this.frame++;
+        this.timer++;
+        if(this.timer % 10 === 0){
+            this.frame++;
+        }
     }
 
     draw(){
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.height / 2, this.width, this.height);
+        ctx.restore();
     }
 }
 
 window.addEventListener('click', function(e){
-    let positionX = e.x - canvasPosition.left;
-    let positionY = e.y - canvasPosition.top;
-    explosions.push(new Explosion(positionX, positionY));
+     createAnimation(e);
 });
+
+window.addEventListener('mousemove', function(e){
+    createAnimation(e);
+})
+
+function createAnimation(e){
+   let positionX = e.x - canvasPosition.left;
+   let positionY = e.y - canvasPosition.top;
+   explosions.push(new Explosion(positionX, positionY));
+   console.log('Explosions: ', explosions);
+}
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < explosions.length; i++){
         explosions[i].update();
         explosions[i].draw();
+        if(explosions[i].frame > 5){
+            explosions.splice(i, 1);
+            i--;
+        }
     }
 
     requestAnimationFrame(animate);
