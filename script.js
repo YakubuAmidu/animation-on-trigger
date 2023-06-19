cf85953a5281a1a2b0c2ce6d7d7996b91c222e2f
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2023-06-18 23:17:28 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 500;
+canvas.height = 700;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(50, 50, 150, 150);
+
+class Explosion {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.image = new Image();
+        this.image.src = 'img/boom1.png';
+    }
+}
